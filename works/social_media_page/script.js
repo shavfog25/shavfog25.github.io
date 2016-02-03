7af2b5c8bd021d4278bac4d730bfc11b9246ee03
Shav Fogarty <shavfog@gmail.com>
2016-02-02 19:11:05 -0500

    finished site

---
+++ works/social_media_page/script.js
@@ -0,0 +1,10 @@
+$( document ).ready( function (){
+	$("#topPanel").animate({"top":"-=150px"}, 2000 );
+	
+	$("#bottomPanel").animate({"bottom":"-=150px"}, 2000 );
+    
+});
+
+$(document).ready(function(){
+    $(".social").fadeToggle(3000);
+});
